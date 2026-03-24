8ebf18863b75809d537f0d2711690ef9fa3e4b1e
unknown <hmaravillo@hotmail.com>
2026-03-23 23:32:14 -0600

    Update utils

---
+++ analysis_normalized/script/create_normalized_graph.py
@@ -0,0 +1,34 @@
+# ===========================================================
+# PACKAGES
+# ===========================================================
+import sys
+import pickle
+
+from pathlib import Path
+ROOT = Path(__file__).resolve().parents[2]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+    
+from icio_community import ICIO_Network
+
+
+# ===========================================================
+# MAIN
+# ===========================================================
+
+SAVE_DIR = ROOT / "analysis_normalized" / "networks"
+
+years = range(1995, 2023)
+icio = {}
+
+for year in years:
+    print(f"YEAR: {year}")
+    icio[year] = ICIO_Network(year,
+                        normalize = True, # NORMALIZED
+                        by_output = False,
+                        RoW = False,
+                        diagonal = True,
+                        diagonal_country = True)
+    # SAVE FULL NETWORK IN .PKL FILE
+    with open(SAVE_DIR / f"g_{year}.pkl", "wb") as f:
+        pickle.dump(icio[year].g, f)
--- icio_community/utils.py
+++ icio_community/utils.py
@@ -3,8 +3,8 @@
 # ===========================================================
 from pandas import read_csv
 from pathlib import Path
+from numpy import sum as np_sum
 from numpy import log2
-from scipy.stats import entropy
 from sklearn.metrics.cluster import contingency_matrix
 
 # ===========================================================
@@ -17,24 +17,25 @@ BASE_DIR = Path(__file__).parent / "codes"
 # ===========================================================
 aux = read_csv(
     BASE_DIR / "countries.csv", 
-    encoding="latin-1",
-    header=None,
+    encoding = "latin-1",
+    header = None,
     index_col = 0,
-    names=["name", "longitude", "latitude"]
+    names = ["name", "longitude", "latitude"]
     )
-countries =sorted( list(aux.index))
+countries =sorted(list(aux.index))
 countries_names = aux["name"].to_dict()
-countries_centers = aux[["longitude", "latitude"]].apply(tuple, axis=1).to_dict()
+countries_centers = aux[["longitude", "latitude"]].apply(tuple, axis = 1)
+countries_centers = countries_centers.to_dict()
 
 # ===========================================================
 # ACTIVITIES DATA
 # ===========================================================
 aux = read_csv(
     BASE_DIR / "demand.csv", 
-    encoding="latin-1",
-    header=None,
+    encoding = "latin-1",
+    header = None,
     index_col = 0,
-    names=["name"]
+    names = ["name"]
     )
 activities = list(aux.index)
 activities_names = aux["name"].to_dict()
@@ -46,6 +47,10 @@ final_demand_names = ['HFCE',  'NPISH',   'GGFC',
 # ===========================================================
 # FUNCTIONS
 # ===========================================================
+def entropy(p):
+    p = p[p > 0]
+    return - np_sum(p * log2(p))
+
 def variation_info(labels_1, labels_2):
     # Contingency matrix (n_ij)
     contingency = contingency_matrix(labels_1, labels_2)
@@ -54,12 +59,12 @@ def variation_info(labels_1, labels_2):
     P_UV = contingency / contingency.sum()
     
     # Calculate marginal distributions
-    P_U = P_UV.sum(axis=1)
-    P_V = P_UV.sum(axis=0)
+    P_U = P_UV.sum(axis = 1)
+    P_V = P_UV.sum(axis = 0)
 
     # Calculate entropy
-    H_U = entropy(P_U, base=2)
-    H_V = entropy(P_V, base=2)
+    H_U = entropy(P_U, base = 2)
+    H_V = entropy(P_V, base = 2)
     
     # Calculate mutual information
     # I(U;V) = sum_{i,j} P(i,j) log_2 (P(i,j) / (P(i)*P(j)))
